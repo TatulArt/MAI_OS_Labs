7a742ff89d7e28befb9a92dc2494af8ae5507120
TatulArt <m33dnightfury@gmail.com>
2025-12-24 12:55:55 +0000

    lab3

---
+++ lab3/include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <unistd.h>
+
+#define USER_INPUT_BUFFERSIZE 4096 - sizeof(ssize_t)
+#define NAME_BUFFERSIZE 128
+
+typedef struct {
+    ssize_t length;
+    char text[USER_INPUT_BUFFERSIZE];
+} shared_data_t;
+
+#endif
